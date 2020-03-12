849d0209966a361cc584664ea2be3da6728830fa
Rumeel Jessamy <rumeelj@gmail.com>
2020-03-12 15:01:32 -0400

    Using JSON Middleware Parser

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -7,9 +7,14 @@ const quoteCards = require('../models/quoteCards');
 const router = express.Router();
 
 router
+    .use('/quoteCards', (req,res, next)=>{
+        console.log({method: req.method, body: req.body})
+        next();
+    })
+    
     .get('/quoteCards',(req,res) => res.send(quoteCards) )
     .post('/quoteCards', (req,res) => {
-        quoteCards.add(req.query.text); 
+        quoteCards.add(req.body.text); 
         res.send(quoteCards[quoteCards.list.length - 1]);
 
     })
